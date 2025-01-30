28f556e710d97086b04e16eda85d4f3ac7f23174
juice176 <andreamunoz176@gmail.com>
2025-01-30 13:20:59 -0600

    Comments

--- qoute_api.py
+++ qoute_api.py
@@ -2,7 +2,7 @@
 import requests
 import sys
 import tkinter as tk
-
+#class to redirect console print statements into Gui system
 class ConsoleRedirector:
     def __init__(self, widget):
         self.widget = widget
@@ -12,10 +12,12 @@ class ConsoleRedirector:
         self.widget.see(tk.END)
     def flush(self):
         pass
+#initialize tkinter gui window
 root = tk.Tk()
 root.title("Qoutes of the Day")
 root.geometry('550x300')
-
+#function for button action 
+#extracts qoute and author from api request
 def clicked():
     response = requests.get("https://thequoteshub.com/api/")
     if response.status_code == 200:
@@ -31,14 +33,13 @@ def clicked():
 
 output_text = tk.Text(root, height=10, width=40, wrap=tk.WORD)
 
-# Redirect console output to the Text widget
 sys.stdout = ConsoleRedirector(output_text)
 
-# Create a Label widget to display the quote and author
 output_label = tk.Label(root, text="Click the button to get a quote!", wraplength=300)
 output_label.grid(column=1, row=5, columnspan=2, pady=10)
+#button
 btn = tk.Button(root, text = "Click for Qoute", fg="green", command= clicked)
 btn.grid(column=1 , row = 1)
-
+#main
 root.mainloop()
 
